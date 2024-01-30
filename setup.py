ffca03d8db2c63cf11b4031c275fb8f949adcf00
iineolineii <leonidsem10@gmail.com>
2024-01-31 02:08:38 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="async_eval",
+    version="0.0.1",
+    packages=find_packages(),
+	python_requires=">=3.12",
+	install_requires=[]
+)
