d69cdb199628648afbcb63fb89291556cbce9d8c
iineolineii <leonidsem10@gmail.com>
2024-02-17 19:24:12 +0300

    Add some documentation

--- async_eval/aeval.py
+++ async_eval/aeval.py
@@ -6,13 +6,19 @@ from uuid import uuid4
 
 @dataclass
 class Session:
-	cached_code: dict[int, str] = field(default_factory=lambda: {})
+	cached_code:    dict[int, str] = field(default_factory=lambda: {})
 	globals: dict[str, typing.Any] = field(default_factory=lambda: {})
-	locals: dict[str, typing.Any] = field(default_factory=lambda: {})
+	locals:  dict[str, typing.Any] = field(default_factory=lambda: {})
 
 	@property
 	def variables(self) -> dict[str, typing.Any]:
 		return self.globals | self.locals
+
+	@variables.setter
+	def variables(self, value: tuple[dict[str, typing.Any], dict[str, typing.Any]]):
+		globals, locals = value
+		self.globals.update(globals)
+		self.locals.update(locals)
 
 
 class AEvaluator:
@@ -38,25 +44,44 @@ class AEvaluator:
 		Returns:
 			Result of code evaluation or `typing.NoReturn` if the result is empty
 		"""
+		glb = glb or {} # Explicitly create a new dictionary for empty globals
+
+		# Store current code in the session
 		code_hash = uuid4().int
 		filename = f"<code> {code_hash}"
 		self.session.cached_code[code_hash] = code
 
+		# Try to execute the code and get the result
 		try:
-			result, variables = await self.evaluate(code_hash, filename, glb, additional_vars, save_vars)
+			result, variables = await self.evaluate(code, filename, glb, additional_vars)
 
+		# Modify traceback for the occurred exception
 		except Exception as e:
 			traceback = self.patch_tb(*sys.exc_info())
 			raise e.with_traceback(traceback)
 
-		self.session.variables.update(variables)
-		return result
+		# Update variables if needed
+		if save_vars:
+			self.session.variables = variables
+
+		# Return proper result
+		return result if not self.empty_result else typing.NoReturn
+
+	async def evaluate(
+		self,
+		code: str,
+		filename: str,
+		_globals: dict[str, typing.Any],
+		_locals: dict[str, typing.Any]
+	) -> tuple[typing.Any, tuple[dict[str, typing.Any], dict[str, typing.Any]]]:
+		...
 
 
+# Basic usage example
 async def main():
 	evaluator = AEvaluator()
 	result = await evaluator.aeval("code")
-	evaluator.variables
+	evaluator.session.variables
 
 if __name__ == "__main__":
 	asyncio.run(main())
