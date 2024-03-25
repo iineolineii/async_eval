8a8d7cb1e2cf07e8834657dd2668b4c27cded9a3
iineolineii <leonidsem10@gmail.com>
2024-03-26 01:15:48 +0300

    Update to version 2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="async_eval",
-    version="0.0.1",
+    version="2.1.0",
     packages=find_packages(),
 	python_requires=">=3.12",
 	install_requires=[]
