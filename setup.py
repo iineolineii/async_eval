9bf6416781c2305e83f5c559bb3412a09428bdb2
iineolineii <leonidsem10@gmail.com>
2024-10-08 11:34:23 +0300

    Fix version metadata

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="async_eval",
-    version="2.2.0.legacy1",
+    version="2.2.0+legacy1",
     packages=find_packages(),
 	python_requires=">=3.9",
 	install_requires=[]
